a7f10a4accd66fb16b618732354a315a33cf17c5
Sebastian Brunner <brunner.sebi4@gmail.com>
2025-06-05 16:29:28 +0200

    read yaml file

--- ui/src/App.vue
+++ ui/src/App.vue
@@ -1,47 +1,28 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import { load as loadYaml} from "js-yaml"
+
+const onChange = (event: any) => {
+  const reader = new FileReader()
+  reader.onload = (ev) => {
+    const text = reader.result
+    console.debug("Raw file text:", text)
+    const yaml = loadYaml(text, "utf8")
+    console.log("YAML object", yaml)
+  }
+  reader.readAsText(event.target.files[0])
+}
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
+    WAUI - WAP Tool UI
   </header>
 
   <main>
-    <TheWelcome />
+    <input type="file" @change="onChange" />
   </main>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
