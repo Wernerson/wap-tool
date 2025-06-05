64ae043f555fcd2c49bdaa49e3f7e084653149b1
Sebastian Brunner <brunner.sebi4@gmail.com>
2025-06-05 17:00:03 +0200

    first simple reactive ui

--- ui/src/App.vue
+++ ui/src/App.vue
@@ -1,6 +1,12 @@
 <script setup lang="ts">
+import {computed, ref} from "vue"
 import { load as loadYaml} from "js-yaml"
 
+type State = {
+  meta: Object
+}
+
+const state = ref(null)
 const onChange = (event: any) => {
   const reader = new FileReader()
   reader.onload = (ev) => {
@@ -8,6 +14,7 @@ const onChange = (event: any) => {
     console.debug("Raw file text:", text)
     const yaml = loadYaml(text, "utf8")
     console.log("YAML object", yaml)
+    state.value = yaml
   }
   reader.readAsText(event.target.files[0])
 }
@@ -15,11 +22,17 @@ const onChange = (event: any) => {
 
 <template>
   <header>
-    WAUI - WAP Tool UI
+    <h1>WAUI - WAP Tool UI</h1>
+    <input type="file" @change="onChange" />
+    <button>Download</button>
   </header>
 
-  <main>
-    <input type="file" @change="onChange" />
+  <main v-if="state">
+    <section>
+      <label for="author">Autor:</label>
+      <input v-model="state.meta.author" placeholder="Autor" />
+      {{ state.meta.author }}
+    </section>
   </main>
 </template>
 
